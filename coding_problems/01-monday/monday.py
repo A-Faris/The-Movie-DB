62c04d4c7ed714564536f5112376521945a8fb4c
Faris Abulula <abululb02@gmail.com>
2024-04-22 14:09:14 +0100

    done

--- coding_problems/01-monday/monday.py
+++ coding_problems/01-monday/monday.py
@@ -1,2 +1,3 @@
 def wave(people: str) -> list[str]:
-    pass
+    return [people[:num] + person.upper() + people[num+1:] for num, person in enumerate(people) if person != " "]
+    # return [people[:num] + people[num].upper() + people[num+1:] for num in range(len(people)) if people[num] != " "]
