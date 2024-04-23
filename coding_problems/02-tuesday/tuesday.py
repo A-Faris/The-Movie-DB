6aba2b782b53e997acaea065e0f97610d920ef61
Faris Abulula <abululb02@gmail.com>
2024-04-23 14:56:07 +0100

    done

--- coding_problems/02-tuesday/tuesday.py
+++ coding_problems/02-tuesday/tuesday.py
@@ -1,2 +1,10 @@
 def diamond(n: int) -> str:
-    pass
+    if n % 2 == 0 or n < 1:
+        return None
+
+    # output = ""
+    # for i in range(-n//2+1, n//2+1):
+    #     output += abs(i)*" " + (n-abs(i)*2)*"*" + "\n"
+    # return output
+
+    return "\n".join([abs(i)*" " + (n-abs(i)*2)*"*" for i in range(-n//2+1, n//2+1)]) + "\n"
