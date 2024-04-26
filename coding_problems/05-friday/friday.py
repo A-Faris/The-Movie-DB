ba210656c2a4ed9f8d988fa12208d730f75a38c0
Faris Abulula <abululb02@gmail.com>
2024-04-26 11:07:35 +0100

    push

--- coding_problems/05-friday/friday.py
+++ coding_problems/05-friday/friday.py
@@ -1,2 +1,18 @@
 def is_valid_IP(strng: str) -> bool:
-    pass
+    if len(strng.split(".")) != 4:
+        return False
+
+    for num in strng.split("."):
+        if not num.isdigit():
+            return False
+        if not 0 <= int(num) <= 255:
+            return False
+        if int(num[0]) == 0 and len(num) > 1:
+            return False
+
+    return True
+
+
+print(is_valid_IP('12.255.56.1'))  # True
+print(is_valid_IP('12.34.56 .1'))  # False
+print(is_valid_IP('123.045.067.089'))  # False
